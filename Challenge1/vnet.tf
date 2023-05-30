d3ca346cdea602208cc2f1bc0aa99240c57acf14
preethabarma <77629130+preethabarma@users.noreply.github.com>
2023-05-30 13:07:24 +0530

    Add files via upload

---
+++ Challenge1/vnet.tf
@@ -0,0 +1,57 @@
+# Create a virtual network
+resource "azurerm_virtual_network" "demo_vnet" {
+  name                = "vnet-test"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+#Create Frontend subnet
+resource "azurerm_subnet" "fe-subnet" {
+  name                 = "fe-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.demo_vnet.name
+  address_prefixes     = ["10.0.0.0/26"]
+  service_endpoints    = ["Microsoft.Web"]
+
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name    = "Microsoft.Web/serverFarms"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action", "Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+    }
+
+
+  }
+
+  lifecycle {
+    ignore_changes = [
+      delegation,
+    ]
+  }
+}
+
+#Create backend subnet
+resource "azurerm_subnet" "be-subnet" {
+  name                 = "be-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.demo_vnet.name
+  address_prefixes     = ["10.0.0.64/26"]
+  service_endpoints    = ["Microsoft.Sql"]
+
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name    = "Microsoft.Web/serverFarms"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action", "Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      delegation,
+    ]
+  }
+}
