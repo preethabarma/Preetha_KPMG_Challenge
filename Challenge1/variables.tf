3d2cfdb921cadfa88d433263fd93b5124edc4934
preethabarma <77629130+preethabarma@users.noreply.github.com>
2023-05-30 13:07:00 +0530

    Add files via upload

---
+++ Challenge1/variables.tf
@@ -0,0 +1,10 @@
+variable "location_rg" {
+  default     = "East US"
+  description = "Resource Group location"
+}
+
+variable "object_id" {
+  default     = "23959414-bf1a-4e03-ab2e-d73ac3bd6904"
+  description = "user object"
+}
+
