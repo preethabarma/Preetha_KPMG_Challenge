2985bc5e7c16db96796d28e1b3154da2897f9019
preethabarma <77629130+preethabarma@users.noreply.github.com>
2023-05-30 13:01:45 +0530

    Update main.tf

--- Challenge1/main.tf
+++ Challenge1/main.tf
@@ -1,1 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0.0"
+    }
+  }
+
+
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstate4736"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
 
